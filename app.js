e96807907e6074e43d95a641c27b663cdec05034
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-12-30 12:29:28 +0530

    dynamic class

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const app = Vue.createApp({
       x: 0,
       y: 0,
       spaceShuttles: [
-        { name: "Voyager 1", launchYear: 1977 },
-        { name: "Voyager 2", launchYear: 1977 },
-        { name: "Apollo 11", launchYear: 1969 },
-        { name: "Artemis", launchYear: 2024 },
+        { name: "Voyager 1", launchYear: 1977, img: "1.jpg", isFav: true },
+        { name: "Voyager 2", launchYear: 1977, img: "2.jpg", isFav: false },
+        { name: "Artemis", launchYear: 2024, img: "3.png", isFav: true },
+        { name: "Apollo 11", launchYear: 1969, img: "4.jpg", isFav: true },
       ],
     };
   },
